bc105dcb1e9b187b8e40c718207fb964989d29fa
abdelfetah18 <nstart0319@gmail.com>
2023-04-06 22:19:04 +0100

    Create a Student Page with styles

--- src/index.php
+++ src/index.php
@@ -4,9 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HelloWorld!</title>
+    <title>Computer Science Departement</title>
+    <link rel="stylesheet" href="/styles/style.css">
 </head>
 <body>
-    <h1>HelloWorld</h1>
+    <div class="container">
+        <div class="banner-wrapper">
+            <img src="/images/cse-banner.jpeg" alt="">
+        </div>
+        <div class="wrapper">
+            <!--
+            <div class="tabs-header">
+                <a active-tab="true" class="selected-tab" href="/?page=teacher">Teacher</a>
+                <a href="/?page=student">Student</a>
+            </div>
+            -->
+            <div class="tabs-content">
+                <form action="">
+                    <input type="text" name="username" id="username" placeholder="Username" />
+                    <input type="password" name="password" id="password" placeholder="Password" />
+                    <button class="btn" type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
---
+++ src/student/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Computer Science Departement</title>
+    <link rel="stylesheet" href="/styles/student.css">
+</head>
+<body>
+    <div class="container">
+        <div class="student-profile">
+            <div class="profile-pic">
+                <img src="/images/student.jpg" alt="">
+            </div>
+            <div class="student-name">Student Name</div>
+            <nav>
+                <a href="#">My Schudeler</a>
+                <a href="#">My Notes</a>
+            </nav>
+        </div>
+        <div class="page-content">
+            <div class="page-header">
+                <div class="page-title">Home</div>
+            </div>
+            <div class="row-wrapper">
+                <div class="card-box-wrapper">
+                    <div class="card-box">
+                        <div class="card-title">Section</div>
+                        <div class="card-content">1</div>
+                    </div>
+                </div>
+
+                <div class="card-box-wrapper">
+                    <div class="card-box">
+                        <div class="card-title">Group</div>
+                        <div class="card-content">4</div>
+                    </div>
+                </div>
+                
+                <div class="card-box-wrapper">
+                    <div class="card-box">
+                        <div class="card-title">Study Year</div>
+                        <div class="card-content">2 Year License</div>
+                    </div>
+                </div>
+                
+                <div class="card-box-wrapper">
+                    <div class="card-box">
+                        <div class="card-title">Collee Year</div>
+                        <div class="card-content">2022/2023</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
