cd543a3c725feecdf985621e242d392fb41dffcb
abdelfetah18 <nstart0319@gmail.com>
2023-05-04 17:53:26 +0100

    Implementing 'Assign Group to a student'.

--- src/admin/students.php
+++ src/admin/students.php
@@ -2,7 +2,23 @@
     include("../../database/db_connection.php");
     include("../../includes/admin/route_protection.php");
 
-    $students_r = $mysqli->query('select students.id as id, first_name, last_name, group_number, count(attendance.id) as absence from students join users on users.id = students.user_id left join groups on group_id = groups.id left join attendance on students.id = attendance.student_id and attendance.student_state = "absence" having students.id is not null;');
+    // This is API to get all the groups that available to a specific acadimic level.
+    if(isset($_GET["acadimic_level_id"])){
+        $groups_r = $mysqli->execute_query("select groups.id as id, group_number, level, speciality_name from groups join acadimic_levels on groups.acadimic_level_id = acadimic_levels.id join specialities on acadimic_levels.speciality_id = specialities.id where acadimic_levels.id = ?;", [$_GET["acadimic_level_id"]]);
+        $groups = json_encode($groups_r->fetch_all(MYSQLI_ASSOC));
+        echo $groups;
+        exit();
+    }
+
+    if(isset($_POST["student_id"]) && isset($_POST["group_id"])){
+        $query_r = $mysqli->execute_query("update students set group_id = ? where id = ?;", [$_POST["group_id"], $_POST["student_id"]]);
+        if(!$query_r){
+            echo "Something went wrong.";
+            exit();
+        }
+    }
+
+    $students_r = $mysqli->query('select students.id as id, students.acadimic_level_id as acadimic_level_id, first_name, last_name, group_number, count(attendance.id) as absence from students join users on users.id = students.user_id left join groups on group_id = groups.id left join attendance on students.id = attendance.student_id and attendance.student_state = "absence" group by students.id having students.id is not null;');
     
 ?>
 <!DOCTYPE html>
@@ -14,6 +30,8 @@
     <title>Computer Science Departement</title>
     <link rel="stylesheet" href="/styles/admin.css">
     <link rel="stylesheet" href="/styles/aside.css">
+    <link rel="stylesheet" href="/styles/dialogue.css">
+    <link rel="stylesheet" href="/styles/forms.css">
     <link rel="stylesheet" href="/styles/list.css">
     <link rel="stylesheet" href="/styles/search.css">
     <link rel="stylesheet" href="/styles/buttons.css">
@@ -56,7 +74,7 @@
                                                     </div>
                                                     <div class="student-info">
                                                         <div class="student-name">'.$row["first_name"].' '.$row["last_name"].'</div>
-                                                        <div class="student-group">Group '.$row["group_number"].'</div>
+                                                        '.($row["group_number"] ? '<div class="student-group">Group '.$row["group_number"].'</div>' : '<div class="small-btn open-dialogue-btn" student_id="'.$row["id"].'" acadimic_level_id="'.$row["acadimic_level_id"].'" style="align-self: self-start; margin-bottom: 4px">Assign Group</div>').'
                                                         <div class="student-grade">'.($row["current_grade"] ? $row["current_grade"] : "Grade not yet").'</div>
                                                         <div class="student-absence">'.$row["absence"].' Absence</div>
                                                     </div>
@@ -73,5 +91,65 @@
             </div>
         </div>
     </div>
+
+    <div id="dialogue" class="dialogue">
+        <div class="dialogue-inner">
+            <div class="dialogue-header">
+                <div class="dialogue-title">Student Group:</div>
+                <div class="dialogue-close-btn" id="dialogue-close-btn">Close</div>
+            </div>
+            <div class="dialogue-body">
+               <div class="row">
+                    <div class="dialogue-student-profile">
+                        <img src="/assets/images/student.jpg" alt="profile_image" />
+                        <div class="student-name" style="width: fit-content; margin-top: 10px;">Abdelfetah Lachenani</div>
+                    </div>
+                    <div class="student-group-select">
+                        <form id="assign-group-form" name="assign-group-form"  method="POST">
+                            <div class="input-wrapper">
+                                <label for="speciality">Groups:</label>
+                                <input list="groups-list" id="group_id" name="group_id" placeholder="group" />
+                                <datalist id="groups-list">
+                                    <!-- Will be loaded using javascript since we have diffrent UI for each student -->
+                                </datalist>
+                            </div>
+                        </form>
+                        <div style="display: flex; flex-direction: row; align-items: center; justify-content: flex-end; width: calc(11/12*100%);">
+                            <div class="cancel-btn dialogue-close-btn">Cancel</div>
+                            <button form="assign-group-form" class="btn" type="submit" style="margin-left: 10px;">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="/assets/js/dialogue.js"></script>
+
+    <script>
+        let btns = document.getElementsByClassName("open-dialogue-btn");
+        for(let i = 0; i < btns.length; i++){
+            let btn = open_dialogue_btns[i];
+            btn.addEventListener("click", (ev) => {
+                let acadimic_level_id = ev.target.getAttribute("acadimic_level_id");
+                let student_id = ev.target.getAttribute("student_id");
+                let assign_group_form = document.getElementById("assign-group-form");
+
+                // FIXME: Using innerHTML can lead to XSS vuln and also it may break the dom tree.
+                //        There is another method, i will use ut later, for now that's it.
+                assign_group_form.innerHTML += "<input type='hidden' id='student_id' name='student_id' value='"+student_id+"' />";
+
+                fetch("/admin/students.php?acadimic_level_id="+acadimic_level_id, {}).then(response => {
+                    response.json().then(data => {
+                        for(let i = 0; i < data.length; i++){
+                            // FIXME: Using innerHTML can lead to XSS vuln and also it may break the dom tree.
+                            //        There is another method, i will use ut later, for now that's it.
+                            document.getElementById("groups-list").innerHTML += ("<option value='"+data[i]['id']+"'>L"+data[i]['level']+" "+data[i]['speciality_name']+" Group "+data[i]['group_number']+"</option>")
+                        }
+                    });
+                }).catch(err => console.log({ error: err }));
+            });
+        }
+    </script>
 </body>
 </html>
